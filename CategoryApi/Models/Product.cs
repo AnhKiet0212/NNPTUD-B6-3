038c4ebe4846414a13e14377f040edca45ac4bae
AnhKiet0212 <anhkiet2122004@gmail.com>
2026-03-06 13:52:41 +0700

    Delete CategoryApi directory

--- CategoryApi/Models/Product.cs
+++
@@ -1,9 +0,0 @@
-namespace CategoryApi.Models;
-
-public class Product
-{
-    public int Id { get; set; }
-    public string Title { get; set; }
-    public decimal Price { get; set; }
-    public int CategoryId { get; set; }
-}
